f0d85ab7094e92f2699cb93a9c4101263413103e
CiaoC <70588807+CiaoC@users.noreply.github.com>
2020-11-30 00:54:37 -0700

    Allow users to see link to GitHub on app

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ export default function App() {
   return (
     <div className="App">
       <Weather />
+      <br></br>
+      <small><a href="https://github.com/CiaoC/weatherweekfour" rel="noreferrer" target="_blank">Open-Source code</a> by Cindy Chau</small>
     </div>
   );
 }
